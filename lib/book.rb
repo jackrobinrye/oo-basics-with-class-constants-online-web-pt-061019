956547caa8cf142d13bc2ffe43559e64c21dc4a9
Jack Atherton <jackrrye@gmail.com>
2019-07-09 16:36:41 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,6 @@
 class Book
 
-  attr_accessor :author, :page_count 
+  attr_accessor :author, :page_count
   attr_reader :title, :genre
 
   GENRES = []
@@ -12,7 +12,7 @@ class Book
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
-  
+
   def genre= (genre)
     @genre = genre
     GENRES << genre
